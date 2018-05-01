39202da43505e73e3c378e23319dec206cff897b
Ben Shoeman <ben.shoeman@outlook.com>
2018-05-01 17:18:49 -0600

    Added -ast flag to allow the AST itself to be printed

--- shell.py
+++ shell.py
@@ -1,13 +1,21 @@
 from eval import evaluate
 from custom_parser import parse
+import sys
 
 def main():
+    ast_mode = False
+    # -ast flag will print the AST instead of the evaluation
+    if len(sys.argv) > 1 and sys.argv[1] == "-ast":
+        ast_mode = True
     # Standard REPL
     while True:
         try:
             expr = input("> ")
             if expr == "exit": break
-            print(evaluate(parse(expr)))
+            if ast_mode:
+                print(parse(expr))
+            else:
+                print(evaluate(parse(expr)))
         except EOFError:
             break
         except Exception as e:
